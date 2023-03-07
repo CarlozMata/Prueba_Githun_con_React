a96d4b32feaabdd508583122355feda7fe1e717c
Carlos Alberto López Mata <mata66666666666@gmail.com>
2023-03-07 11:16:24 -0600

    Updates

---
+++ precache-manifest.628ac6fc5cfb670cc553a1337b65d1b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f982d8a8107db5ede8d9",
+    "url": "/Prueba_Githun_con_React/static/css/main.d1aec279.chunk.css"
+  },
+  {
+    "revision": "f982d8a8107db5ede8d9",
+    "url": "/Prueba_Githun_con_React/static/js/main.f982d8a8.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Prueba_Githun_con_React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/Prueba_Githun_con_React/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "de781613746ba9c455ce",
+    "url": "/Prueba_Githun_con_React/static/js/runtime~main.de781613.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Prueba_Githun_con_React/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6e9bcf1ce1de96d316caf200d388f541",
+    "url": "/Prueba_Githun_con_React/index.html"
+  }
+];
